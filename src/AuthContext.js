19aec247faefc50a8a678ac9baa9a817fb1e2c19
making <9gle3492@gmail.com>
2023-06-04 03:18:45 +0900

    Add log out message

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import Snackbar from '@mui/material/Snackbar';
 import { createContext, useContext, useState } from 'react';
 
 const AuthContext = createContext();
@@ -8,6 +10,7 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
 
   const login = () => {
     setIsLoggedIn(true);
@@ -15,11 +18,22 @@ export const AuthProvider = ({ children }) => {
 
   const logout = () => {
     setIsLoggedIn(false);
+    setSnackbarOpen(true);
   };
 
   return (
     <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
       {children}
+
+      {/* Message */}
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={snackbarOpen}
+        onClose={() => setSnackbarOpen(false)}
+        message="Log out success"
+        autoHideDuration={500}
+        key={'snackbar'}
+      />
     </AuthContext.Provider>
   );
 };
